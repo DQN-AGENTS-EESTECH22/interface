9f6d56fe3024cf48b3361f0d4f6da4bf9b832778
Guilherme Calassi <guilherme@yourmove.com.br>
2022-04-22 22:38:46 +0100

    gulpfile with react

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
 const gulp = require('gulp');
 const rename = require('gulp-rename')
-const browserSync = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass')(require('sass'));
 
-// Compile sass into CSS & auto-inject into browsers
 function scss() {
     return gulp.src("dev/scss/main.scss")
         .pipe(sourcemaps.init())
@@ -15,21 +13,15 @@ function scss() {
         }))
         .pipe(rename('main.min.css'))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest("app/dist/"))
-        .pipe(browserSync.stream());
+        .pipe(gulp.dest("src/"))
 }
 
-// Static Server + watching scss/html files
 function watch() {
-    browserSync.init({
-        server: "./app/"
-    });
-
     gulp.watch("dev/scss/*.scss", gulp.series('scss'));
-    gulp.watch("app/*.html").on('change', browserSync.reload);
 }
 
 exports.scss = scss;
 exports.watch = watch;
 
 gulp.task('default', gulp.series(scss, watch));
+gulp.task('build', gulp.series(watch));
