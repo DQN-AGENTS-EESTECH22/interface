b47e7bb8deb2f78b6f8fdf80a931367685a3fcb1
Guilherme Calassi <guilherme@yourmove.com.br>
2022-04-23 02:48:12 +0100

    restart option

--- src/components/Dropzone.js
+++ src/components/Dropzone.js
@@ -12,6 +12,7 @@ const thumb = {
     display: 'inline-flex',
     marginBottom: 8,
     height: 400,
+    width: '100%',
     padding: 4,
     boxSizing: 'border-box',
     margin: 'auto',
@@ -20,21 +21,13 @@ const thumb = {
 const thumbInner = {
     display: 'flex',
     minWidth: 0,
-    overflow: 'hidden'
+    overflow: 'hidden',
+    margin: 'auto'
 };
-
-const img = {
-    display: 'block',
-    width: 'auto',
-    height: '100%',
-    margin: 'auto',
-};
-
 
 function Dropzone(props) {
-    const [files, setFiles] = useState([]);
     const onDrop = acceptedFiles => {
-        setFiles(acceptedFiles.map(file => Object.assign(file, {
+        props.setFiles(acceptedFiles.map(file => Object.assign(file, {
             preview: window.URL.createObjectURL(file)
         })));
     }
@@ -43,27 +36,26 @@ function Dropzone(props) {
         onDrop: onDrop
     });
 
-    const thumbs = files.map(file => (
+    const thumbs = props.files.map(file => (
         <div style={thumb} key={file.name}>
             <div style={thumbInner}>
                 <img
                     src={file.preview}
-                    style={img}
+                    className={"imageThumb"}
                 />
             </div>
         </div>
     ));
 
     useEffect(() => {
-        // Make sure to revoke the data uris to avoid memory leaks
-        files.forEach(file => URL.revokeObjectURL(file.preview));
-    }, [files]);
+        props.files.forEach(file => URL.revokeObjectURL(file.preview));
+    }, [props.files]);
 
     return (
         <section className="container">
             <div {...getRootProps({className: 'dropzone'})}>
                 <input {...getInputProps()} />
-                <p>Drag 'n' drop some files here, or click to select files</p>
+                <p>Drag 'n drop sua imagem aqui, ou clique para selecionar.</p>
             </div>
             <aside style={thumbsContainer}>
                 {thumbs}
--- src/pages/GeneratePallete.js
+++ src/pages/GeneratePallete.js
@@ -1,11 +1,18 @@
 import Dropzone from "../components/Dropzone";
+import restart from '../restart.png'; // with import
+import {useState} from "react";
 
 
 function GeneratePallete(props) {
+    const [files, setFiles] = useState([]);
+
     return (
         <>
-            <h1>Generate Pallete</h1>
-            <Dropzone/>
+            <h1>Generate Pallete <img onClick={() => setFiles([])} className={'restartImage'} src={restart}/></h1>
+            <Dropzone files={files} setFiles={setFiles}/>
+            {files.length > 0 &&
+            <h1>Pallete</h1>
+            }
         </>
 
     )
