93e4edabe8160d0223b2cffeb40620e393819f24
Caua Veiga <cauaveiga@Cauas-MacBook-Air.local>
2022-04-23 12:51:38 +0100

    worling

--- src/pages/GeneratePallete.js
+++ src/pages/GeneratePallete.js
@@ -27,6 +27,12 @@ function GeneratePalette(props) {
 
     }, [files]);
 
+    useEffect(() => {
+        console.log(colors)
+        let stringRequest = `http://127.0.0.1:9000/update-style?hex1=${colors[0].substring(1)}&hex2=${colors[1].substring(1)}&hex3=${colors[2].substring(1)}&hex4=${colors[3].substring(1)}&hex5=${colors[4].substring(1)}`
+        fetch(stringRequest);
+    }, [colors])
+
     return (
         <>
             <h2>Generate Palette - Image <img alt={"Restart"} onClick={() => setFiles([])} className={'restartImage'} src={restart}/>
--- src/pages/GeneratePalleteText.js
+++ src/pages/GeneratePalleteText.js
@@ -3,9 +3,6 @@ import restart from '../restart.png';
 import {useEffect, useState} from "react"; // with import
 import chroma from "chroma-js";
 import Palette from "../components/Palette";
-//const fs = require('fs');
-
-
 
 
 function GeneratePaletteText(props) {
@@ -22,13 +19,13 @@ function GeneratePaletteText(props) {
             })
             .then(function (res) {
                 setColors(chroma.scale([res[0], res[1]]).colors(5))
-                /* fs.writeFile('../../dev/scss/_variables.scss', 'Hello World!', function (err) {
-                    if (err) return console.log(err);
-                    console.log('Hello World > helloworld.txt');
-                  }); */
             })
-        
     }
+
+    useEffect(() => {
+        let stringRequest = `http://127.0.0.1:9000/update-style?hex1=${colors[0].substring(1)}&hex2=${colors[1].substring(1)}&hex3=${colors[2].substring(1)}&hex4=${colors[3].substring(1)}&hex5=${colors[4].substring(1)}`
+        fetch(stringRequest);
+    }, [colors])
 
     return (
         <>
