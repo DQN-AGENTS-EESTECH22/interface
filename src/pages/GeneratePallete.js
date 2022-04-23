83842f7082d553a7c7df9c755cf0230566dc4962
Guilherme Calassi <guilherme@yourmove.com.br>
2022-04-23 07:12:22 +0100

    fucking /model

--- src/pages/GeneratePallete.js
+++ src/pages/GeneratePallete.js
@@ -9,7 +9,7 @@ function GeneratePallete(props) {
         if (files.length > 0) {
             let data = new FormData()
             data.append('images', files[0]) // maybe it should be '{target}_cand'
-            let url = "https://tongsampah.herokuapp.com"
+            let url = "https://tongsampah.herokuapp.com/model"
             fetch(url, {
                 method: "POST",
                 body: data,
