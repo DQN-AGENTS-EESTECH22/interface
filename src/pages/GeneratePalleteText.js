1b231fa41aeb0ff311ac22633637b6916e451435
Caua Veiga <cauaveiga@Cauas-MacBook-Air.local>
2022-04-23 09:35:25 +0100

    text done

--- src/pages/GeneratePalleteText.js
+++ src/pages/GeneratePalleteText.js
@@ -1,33 +1,44 @@
 import Dropzone from "../components/Dropzone";
 import restart from '../restart.png';
 import {useEffect, useState} from "react"; // with import
+import chroma from "chroma-js";
+
 
 function GeneratePaletteText(props) {
     const [text, setText] = useState('');
-    const [colors, setColors] = useState([[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]])
-    useEffect(() => {
-        if (text.length > 3) {
-
-        }
-    }, [text]);
+    const [colors, setColors] = useState(['#000000', '#000000', '#000000', '#000000', '#000000'])
+    const submit = () => {
+        let url = `http://127.0.0.1:9000/text?search=${text}`
+        fetch(url, {
+            crossDomain: true,
+            method: "GET",
+        })
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (res) {
+                setColors(chroma.scale([res[0], chroma.random()]).colors(5))
+            })
+        
+    }
 
     return (
         <>
             <h1>Generate Palette - Text <img alt={"Restart"} onClick={() => setText('')} className={'restartImage'} src={restart}/>
             </h1>
             <div>
-                <input type={'text'} className={'inputText'} placeholder={'Digite sua busca...'}/>
-                <button className={'inputButton'}>Gerar</button>
+                <input value={text} type={'text'} className={'inputText'} placeholder={'Digite sua busca...'} onChange={evt => setText(evt.target.value)} />
+                <button className={'inputButton'} onClick={submit}>Gerar</button>
             </div>
             {text.length > 3 &&
             <>
                 <h1>Palette</h1>
                 <div className={'colors'}>
-                    <div className={'item'}/>
-                    <div className={'item'}/>
-                    <div className={'item'}/>
-                    <div className={'item'}/>
-                    <div className={'item'}/>
+                    <div className={'item'} style={{ backgroundColor:colors[0] }}/>
+                    <div className={'item'} style={{ backgroundColor:colors[1] }}/>
+                    <div className={'item'} style={{ backgroundColor:colors[2] }}/>
+                    <div className={'item'} style={{ backgroundColor:colors[3] }}/>
+                    <div className={'item'} style={{ backgroundColor:colors[4] }}/>
                 </div>
             </>
             }
