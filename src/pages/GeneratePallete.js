e172c4f8849d6c5a2729e4f4b85d05e3c8682cb6
Guilherme Calassi <guilherme@yourmove.com.br>
2022-04-23 03:58:15 +0100

    just formating

--- src/pages/GeneratePallete.js
+++ src/pages/GeneratePallete.js
@@ -1,24 +1,26 @@
 import Dropzone from "../components/Dropzone";
 import restart from '../restart.png'; // with import
 import {useEffect, useState} from "react";
+
 const {spawn} = require('child_process');
 
 function GeneratePallete(props) {
     const [files, setFiles] = useState([]);
-    const [colors, setColors] = useState([[0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]])
+    const [colors, setColors] = useState([[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]])
 
     useEffect(() => {
-        const python = spawn('python', ['../img_rgb.py','../minion.jpeg']);
+        const python = spawn('python', ['../img_rgb.py', '../minion.jpeg']);
         // collect data from script
         python.stdout.on('data', function (data) {
-        console.log('Pipe data from python script ...');
-        dataToSend = data.toString();
+            console.log('Pipe data from python script ...');
+            let dataToSend = data.toString();
+            console.log(dataToSend)
         });
         // in close event we are sure that stream from child process is closed
         python.on('close', (code) => {
-        console.log(`child process close all stdio with code ${code}`);
-        // send data to browser
-        res.send(dataToSend)
+            console.log(`child process close all stdio with code ${code}`);
+            // send data to browser
+            res.send(dataToSend)
         });
         if (files.length > 0) {
             setColors([
